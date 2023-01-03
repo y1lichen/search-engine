b4d26634a0a2111a1989c74d19ee75963c36661c
Jefferson Chen <f1jefferson@yahoo.com.tw>
2023-01-04 01:40:53 +0800

    add some Chinese predefined keywords

--- src/utils/Filter.java
+++ src/utils/Filter.java
@@ -16,23 +16,33 @@ public class Filter {
 
 	public void initKeywordsList() {
 		keywordsList.add(new Keyword("publisher", 10));
+		keywordsList.add(new Keyword("出版商", 10));
+		keywordsList.add(new Keyword("出版社", 10));
 		keywordsList.add(new Keyword("title", 10));
+		keywordsList.add(new Keyword("書名", 10));
 		keywordsList.add(new Keyword("book name", 10));
 		keywordsList.add(new Keyword("author", 10));
+		keywordsList.add(new Keyword("作者", 10));
+		keywordsList.add(new Keyword("編者", 10));
 		keywordsList.add(new Keyword("isbn", 10));
 		keywordsList.add(new Keyword("series", 10));
 		keywordsList.add(new Keyword("edition", 10));
+		keywordsList.add(new Keyword("電子書", 10));
 		keywordsList.add(new Keyword("epub", 10));
 		keywordsList.add(new Keyword("pdf", 10));
+		keywordsList.add(new Keyword("下載", 10));
 		keywordsList.add(new Keyword("download", 10));
+		keywordsList.add(new Keyword("鏡像", 8));
 		keywordsList.add(new Keyword("mirror", 8));
 		keywordsList.add(new Keyword("mirrors", 8));
 		keywordsList.add(new Keyword("library", 8));
 		keywordsList.add(new Keyword("extension", 7));
+		keywordsList.add(new Keyword("頁數", 6));
 		keywordsList.add(new Keyword("page", 6));
 		keywordsList.add(new Keyword("pages", 6));
 		keywordsList.add(new Keyword("book", 5));
 		keywordsList.add(new Keyword("books", 5));
+		keywordsList.add(new Keyword("語言", 3));
 		keywordsList.add(new Keyword("language", 3));
 	}
 
